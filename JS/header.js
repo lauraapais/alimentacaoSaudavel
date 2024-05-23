ef0ec009b0a79ca53b435364987cf4d7baf07052
Laura Pais <pais.laura@gmail.com>
2024-05-23 15:15:32 +0100

    +

--- JS/header.js
+++ JS/header.js
@@ -1,29 +1,27 @@
-
 //header absolute
 var menuDiv = document.getElementById("menuDiv");
 var menuDesktop = document.getElementById("menuDesktop");
 
 menuDesktop.addEventListener("click", function () {
     menuDiv.classList.add("active");
+    document.body.classList.add("overflow-hidden");
 });
 
 var closeMenu = document.getElementById("closeMenu");
 closeMenu.addEventListener("click", function () {
     menuDiv.classList.remove("active");
+    document.body.classList.remove("overflow-hidden");
 });
 
-
-
-
 //header mobile
-var menuDiv = document.getElementById("menuDiv");
 var menuMobile = document.getElementById("menuMobile");
 
 menuMobile.addEventListener("click", function () {
     menuDiv.classList.add("active");
+    document.body.classList.add("overflow-hidden");
 });
 
-var closeMenu = document.getElementById("closeMenu");
 closeMenu.addEventListener("click", function () {
     menuDiv.classList.remove("active");
+    document.body.classList.remove("overflow-hidden");
 });
